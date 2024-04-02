f86d75964ed6bc2a10d75fb3ace48844fc2f28db
elmurodvokhidov <elmurodvkh@gmail.com>
2024-04-02 11:58:08 +0500

    navbar completed and some bugs fixed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,28 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import MobileNav from './MobileNav'
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <nav className='flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10'>
+            <Link href={"/"} className='flex items-center gap-1'>
+                <Image
+                    src={"/icons/logo.svg"}
+                    alt='yoom logo'
+                    width={32}
+                    height={32}
+                    className='max-sm:size-10'
+                />
+                <p className='text-[26px] font-extrabold text-white max-sm:hidden'>Yoom</p>
+            </Link>
+
+            <div className="flex-between gap-5">
+                {/* Clerk user management */}
+
+                <MobileNav />
+            </div>
+        </nav>
     )
 }
 
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -14,7 +14,7 @@ const Sidebar = () => {
             <div className="flex flex-1 flex-col gap-6">
                 {
                     sidebarLinks.map((link) => {
-                        const isActive = pathname === link.route || pathname.startsWith(link.route);
+                        const isActive = pathname === link.route || pathname.startsWith(`${link.route}/`);
 
                         return (
                             <Link
