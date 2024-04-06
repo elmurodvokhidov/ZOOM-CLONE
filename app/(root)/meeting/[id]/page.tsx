db83ca01470f26aaa7e0579c78f8ca76552b6677
elmurodvokhidov <elmurodvkh@gmail.com>
2024-04-07 00:32:40 +0500

    meeting setup completed

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -20,7 +20,7 @@ const Meeting = ({ params: { id } }: { params: { id: string } }) => {
             <StreamCall call={call}>
                 <StreamTheme>
                     {!isSetupComplete ? (
-                        <MeetingSetup />
+                        <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
                     ) : (
                         <MeetingRoom />
                     )}
--- components/MeetingSetup.tsx
+++ components/MeetingSetup.tsx
@@ -1,6 +1,51 @@
-const MeetingSetup = () => {
+"use client";
+
+import { DeviceSettings, VideoPreview, useCall } from "@stream-io/video-react-sdk"
+import { useEffect, useState } from "react"
+import { Button } from "./ui/button";
+
+const MeetingSetup = ({ setIsSetupComplete }: { setIsSetupComplete: (value: boolean) => void }) => {
+    const [isMicCamToggledOn, setIsMicCamToggledOn] = useState(false);
+
+    const call = useCall();
+
+    if (!call) throw new Error("useCall must be used within StreamCall component!");
+
+    useEffect(() => {
+        if (isMicCamToggledOn) {
+            call?.camera.disable();
+            call?.microphone.disable();
+        }
+        else {
+            call?.camera.enable();
+            call?.microphone.enable();
+        }
+    }, [isMicCamToggledOn, call?.camera, call?.microphone]);
+
     return (
-        <div>MeetingSetup</div>
+        <div className="flex h-screen w-full flex-col items-center justify-center gap-3 text-white">
+            <h1 className="text-2xl font-bold">Setup</h1>
+            <VideoPreview />
+            <div className="flex h-16 items-center justify-center gap-3">
+                <label htmlFor="" className="flex items-center justify-center gap-2 font-medium">
+                    <input
+                        type="checkbox"
+                        checked={isMicCamToggledOn}
+                        onChange={(e) => setIsMicCamToggledOn(e.target.checked)}
+                    />
+                    Join with mic and camera off
+                </label>
+                <DeviceSettings />
+            </div>
+            <Button
+                onClick={() => {
+                    call.join();
+                    setIsSetupComplete(true);
+                }}
+                className="rounded-md bg-green-500 px-4 py-2.5">
+                Join meeting
+            </Button>
+        </div>
     )
 }
 
